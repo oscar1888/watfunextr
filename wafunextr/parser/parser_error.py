19ef4640c4eae54512920930f6a0a0eb300cb888
oscar1888 <oscarx05@outlook.com>
2024-09-18 17:48:31 +0200

    Remove unused error format function in ParserError

--- wafunextr/parser/parser_error.py
+++ wafunextr/parser/parser_error.py
@@ -6,9 +6,5 @@ def _format_unexpected_token(node: Node):
     return f"Syntax error at {node.line}:{node.col}: unexpected {repr(node.token_value) if isinstance(node, Token) else repr('(')}"
 
 
-def _format_expected_token(end_line: int, end_col: int, token_name: str):
-    return f'Syntax error at {end_line}:{end_col}: expecting {str(token_name)}'
-
-
 class ParserError(Exception):
     pass
